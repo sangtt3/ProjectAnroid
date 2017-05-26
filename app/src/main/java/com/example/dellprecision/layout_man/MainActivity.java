7943b2695df6f008653f60371cf2a4c3ddd65086
longgame1989 <longgame1989@gmail.com>
2017-05-26 21:56:14 +0700

    aaaaaaaaaaa

--- app/src/main/java/com/example/dellprecision/layout_man/MainActivity.java
+++ app/src/main/java/com/example/dellprecision/layout_man/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_exercise03);
+        setContentView(R.layout.layout_exercise11);
     }
 }
